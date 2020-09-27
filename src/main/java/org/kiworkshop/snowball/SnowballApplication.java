c144fbbc27d9345daf7ba99472ac36b6cc930531
Kwangil Cho <kwangil.steve.cho@gmail.com>
2020-09-28 08:32:16 +0900

    Initialize springboot project & react project

---
+++ src/main/java/org/kiworkshop/snowball/SnowballApplication.java
@@ -0,0 +1,13 @@
+package org.kiworkshop.snowball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowballApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnowballApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kiworkshop/snowball/SnowballApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kiworkshop.snowball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowballApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
