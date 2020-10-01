d32651591282dd289deb4941127f0ba5524fd190
taesikyoo <yrns0907@gmail.com>
2020-10-01 21:04:50 +0900

    [#1] 유저 도메인 생성

---
+++ src/main/java/org/kiworkshop/snowball/user/domain/User.java
@@ -0,0 +1,35 @@
+package org.kiworkshop.snowball.user.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String name;
+    private int age;
+    private String gender;
+    private String pictureUrl;
+
+    @Builder
+    public User(Long id, String email, String name, int age, String gender, String pictureUrl) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.pictureUrl = pictureUrl;
+    }
+}
