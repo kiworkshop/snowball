c3198af1118befbd2ae107593807e197d1a7816f
ahntree <akh9804@gmail.com>
2020-10-05 11:14:33 +0900

    [#39] 투자노트 작성 배너 추가
    - react-icons 모듈을 통해 유저 아이콘 추가
    - moment 모듈을 통해 현재 날짜 형식 YYYYMMDD로 파싱

---
+++ src/frontend/src/container/note/CreateNoteBannerContainer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import CreateNoteBanner from '../../component/note/CreateNoteBanner';
+
+const CreateNoteBannerWrapperContainer = () => {
+  return <CreateNoteBanner nickname="눈사람" />;
+};
+
+export default CreateNoteBannerWrapperContainer;
