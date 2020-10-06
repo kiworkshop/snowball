b3de623807726c884957ae2a32f8c12c74d5ed9e
taesikyoo <yrns0907@gmail.com>
2020-10-06 23:13:43 +0900

    [#67] Spring REST Docs 설정 추가
    - UserControllerTest에도 관련 설정을 추가
    - ApiDocumentUtils를 test패키지 안에 추가

--- src/test/java/org/kiworkshop/snowball/user/controller/UserControllerTest.java
+++ src/test/java/org/kiworkshop/snowball/user/controller/UserControllerTest.java
@@ -5,26 +5,41 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.kiworkshop.snowball.user.controller.dto.UserCreateRequestDto;
 import org.kiworkshop.snowball.user.domain.User;
 import org.kiworkshop.snowball.user.repository.UserRepository;
 import org.kiworkshop.snowball.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.kiworkshop.snowball.util.ApiDocumentUtils.getDocumentRequest;
+import static org.kiworkshop.snowball.util.ApiDocumentUtils.getDocumentResponse;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "docs.api.com")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UserControllerTest {
 
@@ -40,13 +55,13 @@ class UserControllerTest {
     @Autowired
     private WebApplicationContext context;
 
-    private MockMvc mvc;
+    private MockMvc mockMvc;
 
     @BeforeEach
-    void setup() {
-        mvc = MockMvcBuilders
-                .webAppContextSetup(context)
-                .build();
+    public void setUp(WebApplicationContext webApplicationContext,
+                      RestDocumentationContextProvider restDocumentation) {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation)).build();
     }
 
     @AfterEach
@@ -74,16 +89,33 @@ class UserControllerTest {
         String url = "http://localhost:" + port + "/login";
 
         //when
-        mvc.perform(post(url)
+        ResultActions result = mockMvc.perform(post(url)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().writeValueAsBytes(requestDto)))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .content(new ObjectMapper().writeValueAsBytes(requestDto)));
 
         //then
         User user = userRepository.findByEmail(email).orElseThrow();
         assertThat(user.getName()).isEqualTo(name);
         assertThat(user.getAge()).isEqualTo(age);
         assertThat(user.getGender()).isEqualTo(gender);
+
+        //then
+        result.andExpect(status().isOk())
+                .andDo(document("user/create-user",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        requestFields(
+                                fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
+                                fieldWithPath("name").type(JsonFieldType.STRING).description("유저 이름"),
+                                fieldWithPath("age").type(JsonFieldType.NUMBER).description("나이"),
+                                fieldWithPath("gender").type(JsonFieldType.STRING).description("성별"),
+                                fieldWithPath("pictureUrl").type(JsonFieldType.STRING).description("프로필 사진의 url 주소")
+                        ),
+                        responseFields(
+                                fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
+                                fieldWithPath("pictureUrl").type(JsonFieldType.STRING).description("프로필 사진의 url 주소")
+                        )
+                ));
     }
 
     @DisplayName("유저 조회 시 dto를 반환한다")
@@ -107,7 +139,7 @@ class UserControllerTest {
         String url = "http://localhost:" + port + "/users/" + user.getId();
 
         //when, then
-        mvc.perform(get(url))
+        mockMvc.perform(get(url))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.email", is(email)))
                 .andExpect(jsonPath("$.name", is(name)));
