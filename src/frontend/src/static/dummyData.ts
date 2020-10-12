47ffd9e5d8f927efc5dddbbb2f28f8eff16175bf
안광훈 <akh9804@gmail.com>
2020-10-12 20:22:20 +0900

    [#66] 테스팅 환경 구축 및 의존성 관리
    - referenceerror: mutationobserver is not defined 에러 해결 / jest-environment-jsdom-sixteen 모듈 설치하니까 해결됨
    - typeerror: window.matchmedia is not a function 에러 해결 / setupTests.ts에 mock 설정 코드 추가하니까 해결됨

---
+++ src/frontend/src/static/dummyData.ts
@@ -0,0 +1,42 @@
+export default [
+  {
+    id: '1',
+    text:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos provident repudiandae, architecto doloremque sunt suscipit asperiores accusamus optio fuga quam perspiciatis vero illum illo nemo consectetur dignissimos tempora reiciendis soluta?',
+    investmentDate: '20201001',
+    createdDate: '20201001',
+    lastModifiedDate: '20201001',
+  },
+  {
+    id: '2',
+    text:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos provident repudiandae, architecto doloremque sunt suscipit asperiores accusamus optio fuga quam perspiciatis vero illum illo nemo consectetur dignissimos tempora reiciendis soluta?',
+    investmentDate: '20201002',
+    createdDate: '20201002',
+    lastModifiedDate: '20201002',
+  },
+  {
+    id: '3',
+    text:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos provident repudiandae, architecto doloremque sunt suscipit asperiores accusamus optio fuga quam perspiciatis vero illum illo nemo consectetur dignissimos tempora reiciendis soluta?',
+    investmentDate: '20201003',
+    createdDate: '20201003',
+    lastModifiedDate: '20201003',
+  },
+  {
+    id: '4',
+    text:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos provident repudiandae, architecto doloremque sunt suscipit asperiores accusamus optio fuga quam perspiciatis vero illum illo nemo consectetur dignissimos tempora reiciendis soluta?',
+    investmentDate: '20201004',
+    createdDate: '20201004',
+    lastModifiedDate: '20201004',
+  },
+  {
+    id: '5',
+    text:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos provident repudiandae, architecto doloremque sunt suscipit asperiores accusamus optio fuga quam perspiciatis vero illum illo nemo consectetur dignissimos tempora reiciendis soluta?',
+    investmentDate: '20201005',
+    createdDate: '20201005',
+    lastModifiedDate: '20201005',
+  },
+];
