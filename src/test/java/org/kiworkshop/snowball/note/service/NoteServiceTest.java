857964f93ebee30fbda0d5b798041f12ca7cc469
taesikyoo <yrns0907@gmail.com>
2020-10-13 23:10:34 +0900

    [#16] 투자노트 페이지 조회 테스트 구현

--- src/test/java/org/kiworkshop/snowball/note/service/NoteServiceTest.java
+++ src/test/java/org/kiworkshop/snowball/note/service/NoteServiceTest.java
@@ -11,14 +11,19 @@ import org.kiworkshop.snowball.note.controller.dto.NoteResponseDto;
 import org.kiworkshop.snowball.note.entity.Note;
 import org.kiworkshop.snowball.note.entity.NoteFixture;
 import org.kiworkshop.snowball.note.entity.NoteRepository;
+import org.kiworkshop.snowball.note.entity.PageNoteFixture;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.awt.print.Pageable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -54,18 +59,36 @@ class NoteServiceTest {
     }
 
     @Test
-    void getNotes() {
+    void getNote() {
         //given
-        given(noteRepository.findById(anyLong())).willReturn(Optional.of(NoteFixture.create()));
+        Note note = NoteFixture.create();
+        given(noteRepository.findById(anyLong())).willReturn(Optional.of(note));
         //when
-        NoteResponseDto note = dut.getNote(1L);
+        NoteResponseDto responseDto = dut.getNote(1L);
         //then
-        assertThat(note.getId()).isEqualTo(1L);
-        assertThat(note.getCreatedDate()).isBefore(LocalDateTime.now());
-        assertThat(note.getInvestmentDate()).isBefore(LocalDate.now());
-        assertThat(note.getLastModifiedDate()).isBefore(LocalDateTime.now());
-        assertThat(note.getText()).isEqualTo("NoteFixture");
+        assertThat(responseDto.getId()).isEqualTo(note.getId());
+        assertThat(responseDto.getCreatedDate()).isBefore(LocalDateTime.now());
+        assertThat(responseDto.getInvestmentDate()).isBefore(LocalDate.now());
+        assertThat(responseDto.getLastModifiedDate()).isBefore(LocalDateTime.now());
+        assertThat(responseDto.getText()).isEqualTo(note.getText());
         then(noteRepository).should().findById(anyLong());
+    }
+
+    @Test
+    void getNotes() {
+        // given
+        int page = 1;
+        int size = 2;
+        PageImpl<Note> pageNote = PageNoteFixture.create();
+        given(noteRepository.findAll(any(PageRequest.class))).willReturn(pageNote);
+
+        // when
+        Page<NoteResponseDto> pageNoteResponse = dut.getNotes(PageRequest.of(page, size));
+
+        // then
+        assertThat(pageNoteResponse.getTotalElements()).isEqualTo(pageNote.getSize());
+        assertThat(pageNoteResponse.getContent().get(0).getId()).isEqualTo(1L);
+        assertThat(pageNoteResponse.getContent().get(1).getId()).isEqualTo(2L);
     }
 
     @Test
