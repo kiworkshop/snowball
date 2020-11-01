ab2bc008e01f3d42b7092fdee8d529ce86806466
ahntree <akh9804@gmail.com>
2020-11-01 21:44:59 +0900

    [#94] 노트 리스트 에러 핸들링

--- src/frontend/src/component/note/NoteList.tsx
+++ src/frontend/src/component/note/NoteList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { List, Collapse, Button, Typography, Spin, message } from 'antd';
+import { List, Collapse, Button, Typography, Spin, Alert } from 'antd';
 import { FolderOpenOutlined, RightOutlined } from '@ant-design/icons';
 import { setDate } from '../../lib/date';
 
@@ -37,41 +37,48 @@ const NoteList: React.FC<NoteListProps> = ({
   loading,
   error,
 }) => {
-  if (error) {
-    message.error(error);
-  }
-
   return (
     <Spin tip="로딩중..." spinning={loading}>
-      <List
-        header={
-          <ListHeader>
-            <FolderOpenOutlined style={{ marginRight: '8px' }} />
-            투자노트 목록
-          </ListHeader>
-        }
-        bordered
-        dataSource={notes || []}
-        renderItem={(note) => (
-          <NoteWrapper ghost>
-            <Panel
-              key={note.id}
-              header={
-                note.investmentDate &&
-                `${setDate(note.investmentDate)} 투자노트`
-              }
-            >
-              <Typography.Paragraph>{note.content}</Typography.Paragraph>
-              <MoreInfoButton
-                type="text"
-                onClick={onClickMoreInfoButton(note.id)}
+      {notes && (
+        <List
+          header={
+            <ListHeader>
+              <FolderOpenOutlined style={{ marginRight: '8px' }} />
+              투자노트 목록
+            </ListHeader>
+          }
+          bordered
+          dataSource={notes}
+          renderItem={(note) => (
+            <NoteWrapper ghost>
+              <Panel
+                key={note.id}
+                header={
+                  note.investmentDate &&
+                  `${setDate(note.investmentDate)} 투자노트`
+                }
               >
-                더보기 <RightOutlined />
-              </MoreInfoButton>
-            </Panel>
-          </NoteWrapper>
-        )}
-      />
+                <Typography.Paragraph>{note.content}</Typography.Paragraph>
+                <MoreInfoButton
+                  type="text"
+                  onClick={onClickMoreInfoButton(note.id)}
+                >
+                  더보기 <RightOutlined />
+                </MoreInfoButton>
+              </Panel>
+            </NoteWrapper>
+          )}
+        />
+      )}
+
+      {error && (
+        <Alert
+          type="error"
+          closable
+          message="투자노트 목록을 불러오는 동안 오류가 발생했습니다."
+          style={{ marginTop: '16px' }}
+        />
+      )}
     </Spin>
   );
 };
