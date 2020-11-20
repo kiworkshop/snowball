d31b4efa25b85c5a39619e7a93babdb46b01ce7b
taesikyoo <yrns0907@gmail.com>
2020-11-20 21:41:08 +0900

    [#8] OAuth2 구글 로그인을 위한 설정과 클래스 추가

--- src/main/java/org/kiworkshop/snowball/common/config/WebMvcConfig.java
+++ src/main/java/org/kiworkshop/snowball/common/config/WebMvcConfig.java
@@ -1,15 +1,22 @@
 package org.kiworkshop.snowball.common.config;
 
+import lombok.RequiredArgsConstructor;
+import org.kiworkshop.snowball.common.config.auth.LoginUserArgumentResolver;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://develop.snowball.live")
-                .allowedMethods("*");
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
     }
 }
