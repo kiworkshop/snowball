ddf1869a110f6ec770ece6790bc9b5b36a0dcc0d
ahntree <akh9804@gmail.com>
2020-12-24 23:33:36 +0900

    [#138] login 부분 리팩토링
    - login 컴포넌트 이름 변경
    - 색깔 상수화

--- src/frontend/src/pages/LoginPage.tsx
+++ src/frontend/src/pages/LoginPage.tsx
@@ -3,18 +3,16 @@ import { useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { RootState } from '../store/modules';
 import routes from '../routes';
-
-import LoginContainer from '../container/login/LoginContainer';
+import LoginTemplateContainer from '../container/login/LoginTemplateContainer';
 
 const LoginPage = () => {
-  const isLoggedIn = useSelector((state: RootState) => state.user.isLoggedIn);
+  const { isLoggedIn } = useSelector((state: RootState) => state.user);
 
-  return (
-    <>
-      <LoginContainer />
-      {isLoggedIn && <Redirect to={routes.home()} />}
-    </>
-  );
+  if (isLoggedIn) {
+    return <Redirect to={routes.home()} />
+  }
+
+  return <LoginTemplateContainer />
 };
 
 export default LoginPage;
