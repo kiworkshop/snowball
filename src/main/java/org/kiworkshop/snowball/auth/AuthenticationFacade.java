f81516ca8994ca05697894b705fad20e34beca98
taesikyoo <yrns0907@gmail.com>
2021-01-04 20:02:59 +0900

    [#140] IAuthenticationFacade에서 사용하는 getUser메서드 추가

--- src/main/java/org/kiworkshop/snowball/auth/AuthenticationFacade.java
+++ src/main/java/org/kiworkshop/snowball/auth/AuthenticationFacade.java
@@ -1,14 +1,30 @@
 package org.kiworkshop.snowball.auth;
 
+import lombok.RequiredArgsConstructor;
+import org.kiworkshop.snowball.user.entity.User;
+import org.kiworkshop.snowball.user.entity.UserRepository;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
 public class AuthenticationFacade implements IAuthenticationFacade {
+
+    private final UserRepository userRepository;
 
     @Override
     public Authentication getAuthentication() {
         return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    @Override
+    public User getUser() {
+        Object principal = getAuthentication().getPrincipal();
+        DefaultOAuth2User oAuth2User = (DefaultOAuth2User) principal;
+        String userEmail = oAuth2User.getAttribute("email");
+        User user = userRepository.findByEmail(userEmail).orElseThrow(() -> new IllegalStateException("유저 attribute가 존재하지 않습니다."));
+        return user;
     }
 }
--- src/main/java/org/kiworkshop/snowball/auth/IAuthenticationFacade.java
+++ src/main/java/org/kiworkshop/snowball/auth/IAuthenticationFacade.java
@@ -1,8 +1,11 @@
 package org.kiworkshop.snowball.auth;
 
+import org.kiworkshop.snowball.user.entity.User;
 import org.springframework.security.core.Authentication;
 
 public interface IAuthenticationFacade {
 
     Authentication getAuthentication();
+
+    User getUser();
 }
