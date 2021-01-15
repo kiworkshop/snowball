83c25250a30b4b7dcc76f4a2fed1ea13aa7cf6ce
taesikyoo <yrns0907@gmail.com>
2021-01-15 17:23:22 +0900

    [#139] User API에 /me 추가 및 User 도메인 리팩토링
     - 프론트엔드에서 현재유저가 로그인한 유저인지 확인하기 위해 /me를 추가
     - 그밖에 사용하지 않는 api 메소드들을 제거하고, dto의 이름을 간소화

--- src/main/java/org/kiworkshop/snowball/user/controller/dto/UserResponse.java
+++ src/main/java/org/kiworkshop/snowball/user/controller/dto/UserResponse.java
@@ -4,14 +4,14 @@ import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-public class UserResponseDto {
+public class UserResponse {
 
     private Long id;
     private String name;
     private String pictureUrl;
 
     @Builder
-    public UserResponseDto(Long id, String name, String pictureUrl) {
+    public UserResponse(Long id, String name, String pictureUrl) {
         this.id = id;
         this.name = name;
         this.pictureUrl = pictureUrl;
