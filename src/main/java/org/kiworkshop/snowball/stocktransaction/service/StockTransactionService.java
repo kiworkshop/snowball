29c4e19b3f7bc1287e112663127050e520416777
taesikyoo <yrns0907@gmail.com>
2021-01-18 21:34:56 +0900

    [#147] StockTransaction API 수정 및 테스트 수정

--- src/main/java/org/kiworkshop/snowball/stocktransaction/service/StockTransactionService.java
+++ src/main/java/org/kiworkshop/snowball/stocktransaction/service/StockTransactionService.java
@@ -1,26 +1,27 @@
 package org.kiworkshop.snowball.stocktransaction.service;
 
 import lombok.RequiredArgsConstructor;
+import org.kiworkshop.snowball.auth.IAuthenticationFacade;
 import org.kiworkshop.snowball.common.exception.DomainServiceException;
-import org.kiworkshop.snowball.note.entity.Note;
-import org.kiworkshop.snowball.stocktransaction.dto.StockTransactionAssembler;
-import org.kiworkshop.snowball.stocktransaction.dto.StockTransactionRequestDto;
-import org.kiworkshop.snowball.stocktransaction.dto.StockTransactionCreateResponseDto;
+import org.kiworkshop.snowball.stocktransaction.controller.dto.StockTransactionAssembler;
+import org.kiworkshop.snowball.stocktransaction.controller.dto.StockTransactionRequest;
+import org.kiworkshop.snowball.stocktransaction.controller.dto.StockTransactionCreateResponse;
 import org.kiworkshop.snowball.stocktransaction.entity.StockTransaction;
 import org.kiworkshop.snowball.stocktransaction.entity.StockTransactionRepository;
+import org.kiworkshop.snowball.user.entity.User;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class StockTransactionService {
 
     private final StockTransactionRepository stockTransactionRepository;
+    private final IAuthenticationFacade authenticationFacade;
 
-    public StockTransactionCreateResponseDto create(StockTransactionRequestDto stockTransactionRequestDto) {
-        StockTransaction stockTransaction = stockTransactionRepository.save(StockTransactionAssembler.getStockTransaction(stockTransactionRequestDto));
-        return StockTransactionAssembler.getStockTransactionCreateResponseDto(stockTransaction);
+    public StockTransactionCreateResponse create(StockTransactionRequest stockTransactionRequest) {
+        User user = authenticationFacade.getUser();
+        StockTransaction stockTransaction = stockTransactionRepository.save(StockTransactionAssembler.getStockTransaction(stockTransactionRequest, user));
+        return StockTransactionAssembler.getStockTransactionCreateResponse(stockTransaction);
     }
 
     public StockTransaction get(Long id) {
@@ -31,9 +32,10 @@ public class StockTransactionService {
         return stockTransactionRepository.findById(id).orElseThrow(() -> new DomainServiceException(id + "번의 StockTransaction을 찾을 수 없습니다."));
     }
 
-    public void update(Long id, StockTransactionRequestDto stockTransactionRequestDto) {
+    public void update(Long id, StockTransactionRequest stockTransactionRequest) {
+        User user = authenticationFacade.getUser();
         StockTransaction stockTransaction = getStockTransaction(id);
-        stockTransaction.update(StockTransactionAssembler.getStockTransaction(stockTransactionRequestDto));
+        stockTransaction.update(StockTransactionAssembler.getStockTransaction(stockTransactionRequest, user));
     }
 
     public void delete(Long id) {
