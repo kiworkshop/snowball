48802bf5eac57f7ca1f9e9dc44dcd2a409eb8d79
taesikyoo <yrns0907@gmail.com>
2021-01-18 21:36:31 +0900

    [#147] StockDetail API 일부 수정

---
+++ src/main/java/org/kiworkshop/snowball/stockdetail/controller/dto/StockDetailAssembler.java
@@ -0,0 +1,16 @@
+package org.kiworkshop.snowball.stockdetail.controller.dto;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.kiworkshop.snowball.stockdetail.entity.StockDetail;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class StockDetailAssembler {
+
+    public static StockDetailResponse getStockDetailResponse(StockDetail stockDetail) {
+        return StockDetailResponse.builder()
+                .id(stockDetail.getId())
+                .companyName(stockDetail.getCompanyName())
+                .build();
+    }
+}
