52ee674bcaca545327e26b9ae07baad7270223ac
taesikyoo <yrns0907@gmail.com>
2021-04-05 16:20:04 +0900

    [#190] PortfolioDetail Controller, Service, DTO 생성

---
+++ src/main/java/org/kiworkshop/snowball/portfolio/service/PortfolioDetailService.java
@@ -0,0 +1,14 @@
+package org.kiworkshop.snowball.portfolio.service;
+
+import lombok.RequiredArgsConstructor;
+import org.kiworkshop.snowball.portfolio.controller.dto.PortfolioDetailResponse;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class PortfolioDetailService {
+
+    public PortfolioDetailResponse getPortfolioDetail() {
+        return null;
+    }
+}
