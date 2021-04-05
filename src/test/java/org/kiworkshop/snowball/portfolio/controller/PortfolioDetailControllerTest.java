6498904ef3904717a2a5de2441430bc8fdfd0866
taesikyoo <yrns0907@gmail.com>
2021-04-05 16:20:46 +0900

    [#190] PortfolioDetailControllerTest 추가 및 API docs 생성

---
+++ src/test/java/org/kiworkshop/snowball/portfolio/controller/PortfolioDetailControllerTest.java
@@ -0,0 +1,59 @@
+package org.kiworkshop.snowball.portfolio.controller;
+
+import org.junit.jupiter.api.Test;
+import org.kiworkshop.snowball.ControllerTest;
+import org.kiworkshop.snowball.auth.SecurityConfig;
+import org.kiworkshop.snowball.portfolio.controller.dto.PortfolioDetailResponse;
+import org.kiworkshop.snowball.portfolio.controller.dto.PortfolioDetailResponseFixture;
+import org.kiworkshop.snowball.portfolio.service.PortfolioDetailService;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import static org.kiworkshop.snowball.util.ApiDocumentUtils.getDocumentRequest;
+import static org.kiworkshop.snowball.util.ApiDocumentUtils.getDocumentResponse;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.subsectionWithPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = PortfolioDetailController.class,
+        excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)})
+class PortfolioDetailControllerTest extends ControllerTest {
+
+    @MockBean
+    private PortfolioDetailService portfolioDetailService;
+
+    @WithMockUser(roles = "USER")
+    @Test
+    void getPortfolioDetailTest() throws Exception {
+        // given
+        PortfolioDetailResponse portfolioDetailResponse = PortfolioDetailResponseFixture.create();
+        given(portfolioDetailService.getPortfolioDetail()).willReturn(portfolioDetailResponse);
+
+        // when & then
+        mvc.perform(RestDocumentationRequestBuilders.get("/api/portfolio-detail"))
+                .andExpect(status().isOk())
+                .andDo(document("portfolio/get-portfolio-detail",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        responseFields(
+                                subsectionWithPath("portfolioDetailStocks")
+                                        .type(JsonFieldType.ARRAY).description(""),
+                                subsectionWithPath("portfolioDetailStocks[].companyName")
+                                        .type(JsonFieldType.STRING).description(""),
+                                subsectionWithPath("portfolioDetailStocks[].averageBuyingPrice")
+                                        .type(JsonFieldType.NUMBER).description("매수평균가"),
+                                subsectionWithPath("portfolioDetailStocks[].holdingQuantity")
+                                        .type(JsonFieldType.NUMBER).description("보유수량"),
+                                subsectionWithPath("portfolioDetailStocks[].purchaseAmount")
+                                        .type(JsonFieldType.NUMBER).description("매수금액")
+                        )
+                ));
+    }
+}
