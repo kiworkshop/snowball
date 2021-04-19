8102f188eacbcdb0939bfce55171bdbeb21cc962
ahntree <akh9804@gmail.com>
2021-04-19 11:52:34 +0900

    App 파일 구조 수정 & 로딩 컴포넌트 추가

--- src/frontend/src/AuthApp.tsx
+++ src/frontend/src/AuthApp.tsx
@@ -1,43 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { useAppDispatch, useAppSelector } from './hooks/store';
-import { userSelector } from './lib/selector';
+import styled from 'styled-components';
+import { useAppDispatch, useUserAction, useUserState } from './hooks';
 import history from './lib/history';
 import { getSelectedMenu } from './lib/nav';
-import userSlice from './features/user';
-import Layout from './component/base/Layout';
+import * as Color from './constants/colors';
 import NavContainer from './container/base/NavContainer';
-import AuthAppWrapper from './component/base/AuthAppWrapper';
+import Loading from './component/base/Loading';
 import { CreateNotePage, MainPage, NoteDetailPage, Page404, UpdateNotePage } from './pages';
 
-const AuthApp = () => {
-  /**
-   * redux store
-   */
-  const dispatch = useAppDispatch();
-  const { loading } = useAppSelector(userSelector);
-  const userActions = userSlice.actions;
+const AuthAppContainer = styled.div`
+  background-color: ${Color.GRAY_1};
+  min-height: 100%;
+  min-width: 1190px;
+  width: 100%;
+`;
 
-  const [isInit, setIsInit] = useState(false);
+const ContentContainer = styled.div`
+  margin: 0 auto;
+  padding: 30px 0;
+  width: 1130px;
+`;
+
+const AuthApp = () => {
+  const dispatch = useAppDispatch();
+  const userAction = useUserAction();
+  const { isLoggedIn, loading } = useUserState();
+
   const selectedMenu = [getSelectedMenu(history.location.pathname)];
 
   useEffect(() => {
-    dispatch(userActions.getMeRequest());
-    setIsInit(true);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    dispatch(userAction.getMeRequest());
+  }, [dispatch, userAction]);
 
-  /**
-   * TODO: 로딩 중 화면 UI 만들어서 렌더링 하기
-   */
-  if (!isInit || loading) {
-    return null;
+  if (!isLoggedIn || loading) {
+    return <Loading />;
   }
 
   return (
-    <Layout>
+    <AuthAppContainer>
       <NavContainer selectedMenu={selectedMenu} />
-      <AuthAppWrapper>
+      <ContentContainer>
         <Switch>
           <Route path="/" component={MainPage} exact />
           <Route path="/create/note" component={CreateNotePage} exact />
@@ -45,8 +48,8 @@ const AuthApp = () => {
           <Route path="/note/:id" component={NoteDetailPage} exact />
           <Route component={Page404} />
         </Switch>
-      </AuthAppWrapper>
-    </Layout>
+      </ContentContainer>
+    </AuthAppContainer>
   );
 };
 
