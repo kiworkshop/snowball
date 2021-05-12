3b4fbf6bf4513a341f3e48748af72e3562634797
taesikyoo <yrns0907@gmail.com>
2021-05-12 18:05:30 +0900

    data.sql 수정

--- src/main/resources/db/mariadb/data.sql
+++ src/main/resources/db/mariadb/data.sql
@@ -1,3 +1,8 @@
+ALTER TABLE user convert to charset utf8;
+ALTER TABLE stock_detail convert to charset utf8;
+ALTER TABLE stock_transaction convert to charset utf8;
+ALTER TABLE note convert to charset utf8;
+
 INSERT INTO user (id, email, name, picture_url, created_date, modified_date, role) VALUES (1, 'test-user@snowball.com', '눈사람', '', now(), now(), 'USER');
 
 LOAD DATA LOCAL INFILE '/home/ec2-user/snowball/build/resources/main/KOSPI.csv' REPLACE INTO TABLE `snowball_project`.`stock_detail` COLUMNS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES
