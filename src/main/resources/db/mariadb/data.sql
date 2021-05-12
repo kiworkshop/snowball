a5d10f44d57872285bbdbef04edceef9326fb7f9
taesikyoo <yrns0907@gmail.com>
2021-05-12 17:51:53 +0900

    data.sql 추가

---
+++ src/main/resources/db/mariadb/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO user (id, email, name, picture_url, created_date, modified_date, role) VALUES (1, 'test-user@snowball.com', '눈사람', '', now(), now(), 'USER');
+
+LOAD DATA LOCAL INFILE '/home/ec2-user/snowball/build/resources/main/KOSPI.csv' REPLACE INTO TABLE `snowball_project`.`stock_detail` COLUMNS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES
+(COMPANY_NAME, ITEM_CODE, CATEGORY, MAIN_PRODUCT, LISTING_DATE, SETTLEMENT_MONTH, REPRESENTATIVE);
